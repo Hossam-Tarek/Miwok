3567a4d0ba3161bbf8b0a53f349285d0a440718e
Hossam Tarek <hossamtarek250@gmail.com>
2017-11-12 19:27:55 +0200

    Add family members

--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -17,6 +17,9 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
 
@@ -24,5 +27,27 @@ public class FamilyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
+
+        ArrayList<Word> familyMembers = new ArrayList<Word>();
+        familyMembers.add(new Word("father", "әpә"));
+        familyMembers.add(new Word("mother", "әta"));
+        familyMembers.add(new Word("son", "angsi"));
+        familyMembers.add(new Word("daughter", "tune"));
+        familyMembers.add(new Word("older brother", "taachi"));
+        familyMembers.add(new Word("younger brother", "chalitti"));
+        familyMembers.add(new Word("older sister", "tete"));
+        familyMembers.add(new Word("younger sister", "kolliti"));
+        familyMembers.add(new Word("grandmother", "ama"));
+        familyMembers.add(new Word("grandfather", "paapa"));
+
+
+
+        WordAdapter adapter = new WordAdapter(this, familyMembers);
+
+        // Find the list view and attach it to object
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        // Set adapter to the list view
+        listView.setAdapter(adapter);
     }
 }
