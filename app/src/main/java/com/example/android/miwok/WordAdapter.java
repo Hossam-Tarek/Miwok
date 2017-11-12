453e0469f9d8a4638b3f91fe261cdd2675302396
Hossam Tarek <hossamtarek250@gmail.com>
2017-11-12 18:07:18 +0200

    Implement WordAdapter.java

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -7,16 +7,52 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by hossam on 11/11/17.
+ * Created by Hossam Tarek on 11/11/17.
  */
 
 public class WordAdapter extends ArrayAdapter<Word>
 {
+    /**
+     * @param context The current context. Used to inflate the layout file.
+     * @param words List of Word objects to display in a list.
+     */
+    public WordAdapter(@NonNull Context context, @NonNull ArrayList<Word> words) {
+        super(context, 0, words);
+    }
 
+    /**
+     * Provides a view for AdapterView (ListView, GridView, etc).
+     *
+     * @param position The position in the List of data that should be displayed
+     *                 in the List item view.
+     * @param convertView The recycled view to reuse.
+     * @param parent The parent ViewGroup that is used for inflation.
+     * @return The View for the position in the AdapterView.
+     */
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent)
+    {
+        View listItemView = convertView;
+        if (listItemView == null)
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+
+        Word word = getItem(position);
+
+        TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        miwokTextView.setText(word.getMiwokTranslation());
+
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+        defaultTextView.setText(word.getDefaultTranslation());
+
+        return listItemView;
+    }
 }
 
 
