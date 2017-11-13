db8c1cfa2c144cb744963460ce75b80839493297
Hossam Tarek <hossamtarek250@gmail.com>
2017-11-13 16:24:30 +0200

    Add audio file to each Word object in all categories

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -29,14 +29,22 @@ public class ColorsActivity extends AppCompatActivity {
         setContentView(R.layout.word_list);
 
         ArrayList<Word> colors = new ArrayList<Word>();
-        colors.add(new Word("red", "wetetti", R.drawable.color_red));
-        colors.add(new Word("green", "chokokki", R.drawable.color_green));
-        colors.add(new Word("brown", "takaakki", R.drawable.color_brown));
-        colors.add(new Word("gray", "topoppi", R.drawable.color_gray));
-        colors.add(new Word("black", "kululli", R.drawable.color_black));
-        colors.add(new Word("white", "kelelli", R.drawable.color_white));
-        colors.add(new Word("dusty yellow", "topiisә", R.drawable.color_dusty_yellow));
-        colors.add(new Word("mustard yellow", "chiwiitә", R.drawable.color_dusty_yellow));
+        colors.add(new Word("red", "wetetti", R.drawable.color_red,
+                R.raw.color_red));
+        colors.add(new Word("green", "chokokki", R.drawable.color_green,
+                R.raw.color_green));
+        colors.add(new Word("brown", "takaakki", R.drawable.color_brown,
+                R.raw.color_brown));
+        colors.add(new Word("gray", "topoppi", R.drawable.color_gray,
+                R.raw.color_gray));
+        colors.add(new Word("black", "kululli", R.drawable.color_black,
+                R.raw.color_black));
+        colors.add(new Word("white", "kelelli", R.drawable.color_white,
+                R.raw.color_white));
+        colors.add(new Word("dusty yellow", "topiisә", R.drawable.color_dusty_yellow,
+                R.raw.color_dusty_yellow));
+        colors.add(new Word("mustard yellow", "chiwiitә", R.drawable.color_dusty_yellow,
+                R.raw.color_mustard_yellow));
 
         WordAdapter adapter = new WordAdapter(this, colors, R.color.category_colors);
 
