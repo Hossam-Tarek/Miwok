fb1c67b961c8a648fe5ba0c113c90c9bae1e7182
Hossam Tarek <hossamtarek250@gmail.com>
2017-11-13 16:10:47 +0200

    Modify Word to contain audio resource id

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -12,15 +12,17 @@ public class Word
     private String miwokTranslation;
     private int imageResourceID;
     private static final int NO_IMAGE_PROVIDED = 0;
+    private int audioResourceID;
 
-    public Word(String defaultTranslation, String miwokTranslation, int imageResourceID) {
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceID, int audioResourceID) {
         this.defaultTranslation = defaultTranslation;
         this.miwokTranslation = miwokTranslation;
         this.imageResourceID = imageResourceID;
+        this.audioResourceID = audioResourceID;
     }
 
-    public Word(String defaultTranslation, String miwokTranslation) {
-        this(defaultTranslation, miwokTranslation, NO_IMAGE_PROVIDED);
+    public Word(String defaultTranslation, String miwokTranslation, int audioResourceID) {
+        this(defaultTranslation, miwokTranslation, NO_IMAGE_PROVIDED, audioResourceID);
     }
 
     public String getDefaultTranslation() {
@@ -38,5 +40,10 @@ public class Word
     public boolean hasImage()
     {
         return imageResourceID != NO_IMAGE_PROVIDED;
+    }
+
+    public int getAudioResourceID()
+    {
+        return audioResourceID;
     }
 }
