bd3bbf4d3f3c2af65726c39bffdce18c6d945a7c
Hossam Tarek <hossamtarek250@gmail.com>
2017-11-13 07:27:47 +0200

    Modify colors, family and numbers activity to contain image resource id

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -29,14 +29,14 @@ public class ColorsActivity extends AppCompatActivity {
         setContentView(R.layout.word_list);
 
         ArrayList<Word> colors = new ArrayList<Word>();
-        colors.add(new Word("red", "wetetti"));
-        colors.add(new Word("green", "chokokki"));
-        colors.add(new Word("brown", "takaakki"));
-        colors.add(new Word("gray", "topoppi"));
-        colors.add(new Word("black", "kululli"));
-        colors.add(new Word("white", "kelelli"));
-        colors.add(new Word("dusty yellow", "topiisә"));
-        colors.add(new Word("mustard yellow", "chiwiitә"));
+        colors.add(new Word("red", "wetetti", R.drawable.color_red));
+        colors.add(new Word("green", "chokokki", R.drawable.color_green));
+        colors.add(new Word("brown", "takaakki", R.drawable.color_brown));
+        colors.add(new Word("gray", "topoppi", R.drawable.color_gray));
+        colors.add(new Word("black", "kululli", R.drawable.color_black));
+        colors.add(new Word("white", "kelelli", R.drawable.color_white));
+        colors.add(new Word("dusty yellow", "topiisә", R.drawable.color_dusty_yellow));
+        colors.add(new Word("mustard yellow", "chiwiitә", R.drawable.color_dusty_yellow));
 
         WordAdapter adapter = new WordAdapter(this, colors);
 
