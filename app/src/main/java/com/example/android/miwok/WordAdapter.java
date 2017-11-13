e6e9f68332fb0f228219361d23b3e6e6a5449cfc
Hossam Tarek <hossamtarek250@gmail.com>
2017-11-13 07:49:48 +0200

    Modify WordAdapter to support image view

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -50,6 +51,9 @@ public class WordAdapter extends ArrayAdapter<Word>
 
         TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
         defaultTextView.setText(word.getDefaultTranslation());
+
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.image_view);
+        imageView.setImageResource(word.getImageResourceID());
 
         return listItemView;
     }
